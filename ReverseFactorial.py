d492f97076dce5f0fb7c4019f64015e97319990e
LameCoderVersionTwo <33550344+LameCoderVersionTwo@users.noreply.github.com>
2017-11-21 14:56:11 -0600

    Update ReverseFactorial.py

--- ReverseFactorial.py
+++ ReverseFactorial.py
@@ -1,13 +1,33 @@
 import sys
 import functools
-numberToBeFactorialed = int(input("Number to be reverse factorialed: "))
-while True:
-  endFactorial = numberToBeFactorialed / 2
-  number2 = 2
-  while endFactorial > 1:
-    number2 = number2 + 1
-    endFactorial= endFactorial / number2
-    if endFactorial < 2 > 1:
-      print("The reverse factorial of: ", numberToBeFactorialed, "is", endFactorial + number2 - 1)
-      break
-  break
+import decimal
+type = input("exponent, or normal")
+if type == "normal":
+  numberToBeFactorialed = int(input("Number to be reverse factorialed: "))
+  while True:
+    endFactorial = numberToBeFactorialed / 2
+    number2 = 2
+    while endFactorial > 1:
+      number2 = number2 + 1
+      endFactorial= endFactorial / number2
+      if endFactorial < 2 > 1:
+        print("The reverse factorial of: ", numberToBeFactorialed, "is", endFactorial + number2 - 1)
+        break
+    break
+  
+elif type == "exponent":
+      number1 = int(input("Number below the exponent: "))
+      exponent1 = int(input("Exponent: "))
+      while True:
+        numberToBeFactorialed = number1 ** exponent1
+        endFactorial = numberToBeFactorialed / 2
+        number2 = 2
+        while endFactorial > 1:
+          number2 = number2 + 1
+          endFactorial= endFactorial / number2
+          if endFactorial < 2 > 1:
+            print("The reverse factorial of: ", numberToBeFactorialed, "is", endFactorial + number2 - 1)
+            break
+        break
+else:
+  print ("Please input as 'exponent' or 'normal'")
