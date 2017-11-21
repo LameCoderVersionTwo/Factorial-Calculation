058287ce444a4d14a12b12e852c131e68e13a11f
LameCoderVersionTwo <33550344+LameCoderVersionTwo@users.noreply.github.com>
2017-11-21 12:25:17 -0600

    Update ReverseFactorial.py

--- ReverseFactorial.py
+++ ReverseFactorial.py
@@ -1,16 +1,14 @@
 import sys
 import functools
-numberToBeFactorialed = int(input("Number to be reverse factorialed: "))
-endFactorial = numberToBeFactorialed / 1
-if (endFactorial == numberToBeFactorialed):
-  number1 = 0
-  number2 = 1
-  number2 = number2 + number1 + 1
-  endFactorial = numberToBeFactorialed / number2
-elif endFactorial < numberToBeFactorialed:
-  number2 = number2 + 1
-  endFactorial= endFactorial / number2
-elif endFactorial < 1:
-  print("The reverse factorial of: ", numberToBeFactorialed, "is", endFactorial)
-else:
-  print('Cant becalculated')
+while True:
+  numberToBeFactorialed = int(input("Number to be reverse factorialed: "))
+  endFactorial = numberToBeFactorialed / 2
+  number2 = 2
+  if endFactorial > 1:
+    number2 = number2 + 1
+    endFactorial= endFactorial / number2
+    if endFactorial == 1:
+      print("The reverse factorial of: ", numberToBeFactorialed, "is", number2)
+  else:
+    print('Cant be calculated')
+  
