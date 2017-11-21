0db1fbc6f7605e7842999b04a598264a54b78d96
LameCoderVersionTwo <33550344+LameCoderVersionTwo@users.noreply.github.com>
2017-11-21 11:34:03 -0600

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,33 +1,25 @@
+import sys
+import datetime
+import operator
+import functools
+import decimal
 maxNumber = int(input('What will the maximum count be?: '))
+#This number will dictate how long the program will go (suggested under 100k or it may #take large amounts of time
 numberIncrease = int(input('What will be the number added per round?: '))
+#the code starts at zero and adds this number to the previous number to calculate the
+#factorial of the number
 number1 = 0
 while (number1 < maxNumber):
-	import sys
+	now = datetime.datetime.now()
 	number2=number1+1
 	number1=number1+numberIncrease
 	multipliers=list()
-	import datetime
-	now = datetime.datetime.now()
-	sys.stdout=open("Factorial.txt","a")
-	print ("Start time", now.strftime('%H%m%S.%f'))
-	import sys
-	sys.stdout.close()
 	for i in range(number1,0,-1):
 			multipliers.append(i)
-			#print(i)
-	import operator
-	import functools
-	import decimal
 	result = functools.reduce(operator.mul,multipliers, 1)
-	import datetime
-	now1 = datetime.datetime.now()
-	sys.stdout=open("Factorial.txt","a")
-	print ("End time", now1.strftime('%H%m%S.%f'))
-	sys.stdout.close()
 	x = decimal.Decimal(result)
-	sys.stdout=open("Factorial.txt","a")
+	now1 = datetime.datetime.now()
+	print ("Start time", now.strftime('%H%m%S.%f'))
+	print ("End time", now1.strftime('%H%m%S.%f'))
 	print("Factorial of", number1, "is", format(x, '.6e'))
-	sys.stdout.close()
-	sys.stdout=open("Factorial.txt","a")
 	print("The time difference is", (now1 - now).total_seconds(), "seconds")
-	sys.stdout.close()
