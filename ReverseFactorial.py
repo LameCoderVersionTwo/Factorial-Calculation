0b7c14225a2044e7dfd732e955556bfcfb594771
LameCoderVersionTwo <33550344+LameCoderVersionTwo@users.noreply.github.com>
2017-11-21 11:12:07 -0600

    Finds original factorial of a number
    This program will find a way to simplify a massive number into factorial form.

---
+++ ReverseFactorial.py
@@ -0,0 +1,15 @@
+import sys
+import functools
+numberToBeFactorialed = int(input("Number to be reverse factorialed: ")
+while True:
+  if (endFactorial = numberToBeFactorialed):
+    number1 = 0
+    number2 = number 2 + number1 + 1
+    endFactorial = numberToBeFactorialed / number2
+  elif (endFactorial < numberToBeFactorialed):
+    number2 = number2 + 1
+    endFactorial= endFactorial / number2
+  else (endFactorial < 1):
+    sys.stdout=open("ReverseFactorial.txt","a")
+    print("The reverse factorial of: " numberToBeFactorialed, "is", endFactorial)
+    sys.stdout.close()
